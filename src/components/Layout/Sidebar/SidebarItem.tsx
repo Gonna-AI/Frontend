a897fd0f0c341038c6a3f069a8f4baab79ffc9ff
amethystani <animeshmishra0567@gmail.com>
2024-12-25 20:40:11 +0530

    edited the sidebar bug

--- src/components/Layout/Sidebar/SidebarItem.tsx
+++ src/components/Layout/Sidebar/SidebarItem.tsx
@@ -24,7 +24,7 @@ export default function SidebarItem({
   const isActive = to ? location.pathname === to : false;
 
   const commonClasses = cn(
-    "w-full flex items-center p-3 rounded-xl",
+    "w-full px-4 py-2 rounded-lg flex items-center gap-3",
     "transition-colors duration-300",
     isDark
       ? "hover:bg-white/5 text-white/80 hover:text-white"
