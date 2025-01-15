80419a95609db45a21ae2228daed8dafbdb5713d
amethystani <animeshmishra0567@gmail.com>
2025-01-15 12:22:17 +0530

    fixed config

--- src/components/Auth/PrivateRoute.tsx
+++ src/components/Auth/PrivateRoute.tsx
@@ -1,6 +1,6 @@
 import { Navigate } from 'react-router-dom';
 import { useEffect, useState } from 'react';
-import api from '../config/api';
+import api from '../../config/api';
 
 interface PrivateRouteProps {
     children: React.ReactNode;
