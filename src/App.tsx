2470d798b140f01b00f9c639b9b6e90fa6fed31b
amethystani <animeshmishra0567@gmail.com>
2025-11-14 14:48:41 +0530

    Add About and Careers pages; update navigation links in Hero and Contact components

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,8 @@ import SmartContracts from './pages/SmartContracts';
 import Documents from './pages/Documents';
 import Contact from './pages/Contact';
 import InvitePage from './pages/InvitePage';
+import About from './pages/About';
+import Careers from './pages/Careers';
 import { PrivateRoute } from './components/Auth/PrivateRoute';
 import LoadingScreen from './components/LoadingScreen';
 // Create a client
@@ -162,6 +164,8 @@ function App() {
               <AISettings />
             </ProtectedRoute>
           } />
+          <Route path="/about" element={<About />} />
+          <Route path="/careers" element={<Careers />} />
           <Route path="/profile" element={
             <ProtectedRoute>
               <Profile />
