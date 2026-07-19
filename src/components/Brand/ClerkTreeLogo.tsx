e7b682cf0490db33cfd71b1e9a9a98b5b6bbaa74
amethystani <animeshmishra0567@gmail.com>
2026-07-19 12:33:53 +0530

    style: fix logo mark bottom clipping by adjusting viewBox

--- src/components/Brand/ClerkTreeLogo.tsx
+++ src/components/Brand/ClerkTreeLogo.tsx
@@ -14,7 +14,7 @@ export default function ClerkTreeLogo({
       <svg
         className={markClassName}
         xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 464 468"
+        viewBox="0 -10 464 488"
         aria-hidden="true"
         focusable="false"
       >
